79dab3f29a844fc0c726768418fbeeb94fbcd67b
npullen2000 <80427203+npullen2000@users.noreply.github.com>
2023-01-16 13:25:45 -0600

    Update OSSECMacOSLibs.sh

--- OSSECMacOSLibs.sh
+++ OSSECMacOSLibs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 apt update  # To get the latest package lists
 apt upgrade -y
+wget -U ossec https://github.com/ossec/ossec-hids/archive/3.7.0.tar.gz
 brew install build-essential
 brew install libevent-dev -Y
 brew install libpcre2-dev -Y
