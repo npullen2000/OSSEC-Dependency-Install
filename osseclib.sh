6e005ccad2891b7c2d5bb031ac3c70bed7051c89
npullen2000 <80427203+npullen2000@users.noreply.github.com>
2023-01-16 00:57:46 -0600

    Update osseclib.sh

--- osseclib.sh
+++ osseclib.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 apt update  # To get the latest package lists
 apt upgrade -y
+wget -U ossec https://github.com/ossec/ossec-hids/archive/3.7.0.tar.gz
 apt-get install build-essential
 apt-get install libevent-dev -Y
 apt-get install libpcre2-dev -Y
